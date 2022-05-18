90936eaba17352002045015bed7950f9db19de23
galihgpr <galihprayogi25@gmail.com>
2022-05-18 14:36:38 +0700

    Check Api

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"event/entities"
+	"fmt"
 
 	"github.com/labstack/gommon/log"
 	"gorm.io/driver/mysql"
@@ -9,16 +10,15 @@ import (
 )
 
 func InitDB() *gorm.DB {
-	// config := InitConfig()
+	config := InitConfig()
 
-	// conString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
-	// 	config.Username,
-	// 	config.Password,
-	// 	config.Address,
-	// 	config.DB_Port,
-	// 	config.Name,
-	// )
-	conString := "root:Kuroko25nara@tcp(db-learn.cb8meadbge6r.ap-southeast-1.rds.amazonaws.com:3306)/Event?charset=utf8mb4&parseTime=True"
+	conString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		config.Username,
+		config.Password,
+		config.Address,
+		config.DB_Port,
+		config.Name,
+	)
 	db, err := gorm.Open(mysql.Open(conString), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
